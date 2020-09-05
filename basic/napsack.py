d3d12d0bcacd0ddb745d7fc04f0ec96b2d0567b9
xinkent <xinkent1994@gmail.com>
2020-09-05 18:09:23 +0900

    update napsack.py

--- basic/napsack.py
+++ basic/napsack.py
@@ -1,4 +1,7 @@
 """
+# 参考
+https://qrunch.net/@koralle/entries/QaGGA1m4l6NALqLX
+
 # 概要
 動的計画法を使う有名な問題です。以下のような問に答えます。
 
@@ -34,6 +37,8 @@ w_n v_n
 3657162058
 """
 
+import time
+import numpy as np
 
 class SimpleDFS:
     """
@@ -61,14 +66,76 @@ class SimpleDFS:
             )
             return ans 
 
-    def dfs_solve(self):
+    def solve(self):
         print(self.rec(0, W))
+
+class MemoDFS:
+    """
+    メモ化DFS
+    """
+    def __init__(self, N, W, wv):
+        self.N = N
+        self.W = W
+        self.wv = wv
+        self.memo = np.zeros((self.N+1, self.W+1), dtype=int)
+
+    def rec(self, n, uplimit_w):
+        # 既に記録済みならその値を返す
+        if self.memo[n, uplimit_w] > 0:
+            return self.memo[n, uplimit_w]
+
+        ans = 0
+        if n == N:
+            return 0
+
+        # n番目の品物を使えない場合
+        if wv[n][0] > uplimit_w:
+            ans =  self.rec(n + 1, uplimit_w)
+
+        # n番目の品物を使える場合
+        else:
+            ans = max(
+                self.rec(n + 1, uplimit_w),
+                self.rec(n+1, uplimit_w - wv[n][0]) + wv[n][1]
+            )
+
+        self.memo[n, uplimit_w] = ans
+        return ans
+
+class DP:
+    """
+    primitiveなDFS
+    """
+    def __init__(self, N, W, wv):
+        self.N = N
+        self.W = W
+        self.wv = wv
+        self.dp = np.zeros((self.N+1, self.W+1), dtype=int)
+
+    def solve(self):
+        for n in range(0,self.N):
+            for w in range(self.W, -1, -1):
+                # print(n, w)
+                if w + self.wv[n+1, 0] > self.W:
+                    self.dp[n+1, w] = self.dp[n, w]
+                else:
+                    self.dp[n+1, w] = max(self.dp[n, w], self.dp[n, w+self.wv[n+1,0]] + self.wv[n+1, 1])
+                # print(self.dp)
+        print(self.dp[N, 0])
+        # print(self.dp)
 
 
 if __name__ == "__main__":
     N, W = map(int, input().split())
-    wv = [ tuple(map(int, input().split())) for i in range(N)]
-    solver = SimpleDFS(N, W, wv)
-    solver.dfs_solve()
+    wv = np.array([[0,0]] + [ list(map(int, input().split())) for i in range(N)])
+    start = time.time()
+
+    # solver = SimpleDFS(N, W, wv)
+    # solver = MemoDFS(N, W, wv)
+    solver = DP(N, W, wv)
+    solver.solve()
+
+    elapsed_time = time.time() - start
+    print ("elapsed_time:{0}".format(elapsed_time) + "[sec]")
     
 
